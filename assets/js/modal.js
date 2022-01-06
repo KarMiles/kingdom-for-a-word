4bc85babbb807778b5a94439264e35483f931481
Karol Mileszko <karol.miles@gmail.com>
2022-01-06 11:55:01 +0000

    Add Close button to Feedback Modal, remove X

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,6 +3,8 @@
  * to manipulate modals
  */
 
+
+
 // INSTRUCTIONS MODAL
 // Get the modal
 var modalInstr = document.getElementById("modalInstructions");
@@ -11,11 +13,6 @@ var modalInstr = document.getElementById("modalInstructions");
 function startModalInstructions() {
     modalInstr.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modalInstr.style.display = "none";
-// }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -30,28 +27,46 @@ var modalFeedb = document.getElementById("modalFeedback");
 
 // Start feedback modal after given number of games 
 function startModalFeedback() {
+    // set counter
     let oldSuccessCount = parseInt(document.getElementById("success-count").innerText);
     let oldFailCount = parseInt(document.getElementById("fail-count").innerText);
     let counter = oldSuccessCount + oldFailCount;
     console.log(counter);
+    // set allowed number of sessions in a game
     allowedGamesInSession = 2;
+    // send modal to screen after set amout of games
     if(counter >= allowedGamesInSession) {
         modalFeedb.style.display = "block";
     }
+
+    // send feedback info
+    document.getElementById("wins").innerHTML = oldSuccessCount;
+    document.getElementById("sum").innerHTML = oldSuccessCount + oldFailCount;
+    if(oldSuccessCount > oldFailCount) {
+        document.getElementById("startFeedback").innerHTML = "Bravo!"
+        document.getElementById("endFeedback").innerHTML = "Well done!"
+    } else {
+        document.getElementById("startFeedback").innerHTML = "The thing is..."
+        document.getElementById("endFeedback").innerHTML = "Better luck next time!"
+    }
+
+    // reset screen
+    document.getElementById("start-screen").style.display = "initial";
+    document.getElementById("game-screen").style.display = "none";
 }
 
-// When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modalFeedb.style.display = "none";
-// }
+// CLOSING THE FEEDBACK MODAL
+// Get the button that closes the modal
+var closeFeedbackModal = document.getElementById("closeFeedbackBtn");
+
+// When the user clicks on Close button, close the modal
+closeFeedbackModal.onclick = function() {
+    modalFeedback.style.display = "none";
+}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modalFeedb) {
-    modalFeedb.style.display = "none";
+    if (event.target == modalFeedback) {
+    modalFeedback.style.display = "none";
     }
 }
-
-// ALL MODALS
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
